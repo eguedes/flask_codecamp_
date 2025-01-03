02b552d40e3d302a3994fde821f3ca3d6a27d8de
Erick Guedes <sp2oixywc@mozmail.com>
2025-01-03 11:25:11 -0300

    Landing-page

--- app/views.py
+++ app/views.py
@@ -1,6 +1,25 @@
 from app import app
-from flask import render_template
+from flask import render_template, url_for
 
 @app.route('/')
 def homepage():
-    return render_template('index.html')
+    usuario = "Erick"
+    idade = 32
+
+    context = {
+        'usuario': usuario,
+        'idade': idade 
+    }
+    return render_template('index.html', context=context)
+
+@app.route('/nova/')
+def novapag():
+    return "Outra view"
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact/')
+def contact():
+    return render_template('contact.html')
